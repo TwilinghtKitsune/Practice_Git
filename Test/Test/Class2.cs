7a58c11a536a84f4217b8a6667b711a6b3f93447
Ekaterina <Eamaltseva14@gmail.com>
2022-12-13 12:07:34 +0400

    Commit3

---
+++ Test/Test/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Class2
+    {
+        public static void Print(string str)
+        {
+            Console.WriteLine("Hello " + str + "!");
+        }
+    }
+}
